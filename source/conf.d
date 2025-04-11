b7f94305fa7402759fb7bd9a00c5e54e27570b73
PersonWhoExists <Landon.kuehner@proton.me>
2025-04-10 22:02:39 -0400

    Continue work on conf.d

--- source/conf.d
+++ source/conf.d
@@ -8,44 +8,82 @@ import std.path;
 import std.json;
 import std.typecons: tuple;
 
-alias confFormat = tuple!(
-	string[string][], "plugins",
-	bool[string], "settings"
-);
+struct Plugin {
+	string name;
+	string source;
+	string head;
+}
 
-string[string] loadConf() {
+struct PromptComponent {
+	string id;
+	JSONValue config;
+	bool constant;
+}
+
+struct ConfData {
+	Plugin[] pluginList;
+	PromptComponent[] prompt;
+	JSONValue settings;
+}
+
+ConfData parseConf() {
+	Plugin[] pluginList;
+	PromptComponent[] prompt;
 	string confPath;
-	string[string] settings;
+
+	// Blame windows for the next 9 lines
 	version(Windows) {
-		// Fix this, windows doesn't support ~'s
-		confPath = "~\\AppData\\Local\\promptkit\\config.json";
+		import std.process :environment;
+		import std.format;
+		// Screw windows
+		string winConfPath = environment.get("%appdata%");
+		confPath = format("%s\\promptkit\\config.json", winConfPath);
 	}
 	else version(Posix) {
 		confPath = expandTilde("~/.config/promptkit/config.json");
 	}
+
 	if (!(exists(confPath) && isFile(confPath))) {
 		throw new Exception("Please create a conf file at ", confPath);
 	}
+
+	// Parse config file
 	string JSONStr = readText(confPath);
+	JSONValue jsonConfig = parseJSON(JSONStr);
 
-	auto jsonConfig = parseJSON(JSONStr);
-
-	JSONValue[] plugins = jsonConfig["plugins"].array;
-	string[string][] pluginList;
-
-	foreach (plugin; plugins) {
-		string[string] currentPlugin;
-
-		foreach(string key, JSONValue value; plugin) {
-			currentPlugin[key] = value.str();
+	// Populate pluginList with plugin data
+	foreach(JSONValue plugin; jsonConfig["plugins"].array) {
+		string[string] pluginArrMap;
+		foreach(string key; plugin.object.keys) {
+			string value = plugin.object[key].str();
+			pluginArrMap[key] = value;
 		}
+		// Replace this with something dynamic
+		Plugin thisPlugin;
+		thisPlugin.name = pluginArrMap["name"];
+		thisPlugin.source = pluginArrMap["source"];
+		thisPlugin.head = pluginArrMap["head"];
 
-		pluginList ~= currentPlugin;
+		pluginList ~= thisPlugin;
 	}
 
-	foreach(string key, JSONValue value; jsonConfig["settings"]) {
-		settings[key] = value.str();
+	// Populate the prompt with prompt components
+	foreach(JSONValue plugin; jsonConfig["prompt"].array) {
+		string[string] prompComponentArrMap;
+		foreach(string key; plugin.object.keys) {
+			string value = plugin.object[key].str();
+			prompComponentArrMap[key] = value;
+		}
+		// Replace this with something dynamic
+		PromptComponent thisPromptComponent;
+		thisPromptComponent.id = prompComponentArrMap["name"];
+		thisPromptComponent.config = prompComponentArrMap["source"];
+		thisPromptComponent.constant = to!bool(prompComponentArrMap["head"]);
+
+		prompt ~= thisPromptComponent;
 	}
 
-	return settings;
+	auto parsedFile = ConfData(pluginList, prompt, jsonConfig["settings"]);
+
+	return parsedFile;
 }
